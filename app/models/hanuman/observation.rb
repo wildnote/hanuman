c1989ceae65901a6d0e5e7a493660facf812311f
Kristen Hazard <kristen@hazardbio.com>
2014-03-04 12:46:57 -0800

    clean up observation build to not add question attributes

--- app/models/hanuman/observation.rb
+++ app/models/hanuman/observation.rb
@@ -3,8 +3,5 @@ module Hanuman
     belongs_to :survey
     belongs_to :survey_question
 
-    # had to add question attributes to observation attr_accessor so that I could set that in build of controller
-    attr_accessor :question_text, :answer_type, :order, :duplicator, :group
-
   end
 end
