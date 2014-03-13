4472beb0538ec2927a47d6a8a67f9978a5db400c
Kristen Hazard <kristen@hazardbio.com>
2014-03-13 12:21:30 -0700

    add default scope for sort ordering

--- app/models/hanuman/observation.rb
+++ app/models/hanuman/observation.rb
@@ -2,5 +2,6 @@ module Hanuman
   class Observation < ActiveRecord::Base
     belongs_to :survey
     belongs_to :survey_question
+    default_scope  includes(:survey_question).order('hanuman_survey_questions.order asc')
   end
 end
--- app/models/hanuman/survey_question.rb
+++ app/models/hanuman/survey_question.rb
@@ -2,5 +2,7 @@ module Hanuman
   class SurveyQuestion < ActiveRecord::Base
     belongs_to :survey_template
     belongs_to :question
+    has_many :observations
+    default_scope  { order(:order => :asc) }
   end
 end
