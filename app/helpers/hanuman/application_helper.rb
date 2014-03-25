0f5a522a911c0595931ad01997abc3c7cf64b096
David Simmons <david@digitalconcerto.com>
2014-03-24 21:23:58 -0700

    refactor wicked views to share survey form partial and add supporting helpers, update rails 4.0.0 to rails 4.0.2

--- app/helpers/hanuman/application_helper.rb
+++ app/helpers/hanuman/application_helper.rb
@@ -7,5 +7,37 @@ module Hanuman
         super
       end
     end
+    
+    def build_eval_steps_array step
+      array = []
+      case step
+      when 'step_2'
+        array << 'step_1'
+      when 'step_3'
+        array << 'step_1'
+        array << 'step 2'
+      end
+      array
+    end
+    
+    def survey_form_url step
+      case step
+      when 'step_2', 'step_3'
+        :wizard
+      else
+        @survey
+      end
+    end
+    
+    def survey_form_method step
+      case step
+      when 'step_2', 'step_3'
+        :put
+      when 'step_1',
+        :post
+      else
+        :patch
+      end
+    end
   end
 end
--- hanuman.gemspec
+++ hanuman.gemspec
@@ -16,8 +16,8 @@ Gem::Specification.new do |s|
   s.files = Dir["{app,config,db,lib,vendor}/**/*", "MIT-LICENSE", "Rakefile", "README.rdoc"]
   s.test_files = Dir["test/**/*"]
 
-  s.add_dependency 'rails', '4.0.0'
-  s.add_dependency 'arel', '4.0.0'
+  s.add_dependency 'rails', '4.0.2'
+  s.add_dependency 'arel', '4.0.2'
   s.add_dependency 'ancestry'
   s.add_dependency 'wicked'
   s.add_dependency 'haml-rails'
