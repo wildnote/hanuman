88469767ced1e146298e08d38762e2121d442c97
Kristen Hazard <kristen@hazardbio.com>
2014-03-25 16:16:32 -0700

    add method for option text plus scientific text

--- app/models/hanuman/answer_choice.rb
+++ app/models/hanuman/answer_choice.rb
@@ -2,5 +2,9 @@ module Hanuman
   class AnswerChoice < ActiveRecord::Base
     has_ancestry
     belongs_to :question
+
+    def option_text_plus_scientific_text
+      option_text + ' [' + scientific_text + ']'
+    end
   end
 end
