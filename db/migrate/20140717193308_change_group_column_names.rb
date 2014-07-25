e483b2df98f73945d02560a8ca9b9bee876641da
David Simmons <david@digitalconcerto.com>
2014-07-24 19:21:48 -0700

    change drop_column to remove_column in latest migration

--- db/migrate/20140717193308_change_group_column_names.rb
+++ db/migrate/20140717193308_change_group_column_names.rb
@@ -1,7 +1,7 @@
 class ChangeGroupColumnNames < ActiveRecord::Migration
   def change
     add_column :hanuman_survey_questions, :step, :integer
-    drop_column :hanuman_survey_questions, :group
+    remove_column :hanuman_survey_questions, :group
     change_table :hanuman_observations do |t|
       t.rename :group, :set
     end
