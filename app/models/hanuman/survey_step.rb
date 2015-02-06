6a3f0de700a76db3bdc6f0081ccf8d49401c5bc5
David Simmons <david@digitalconcerto.com>
2015-02-05 21:47:35 -0800

    added uniqueness validation to survey step so that we don't allow repeat steps for a particular template id

--- app/models/hanuman/survey_step.rb
+++ app/models/hanuman/survey_step.rb
@@ -4,6 +4,7 @@ module Hanuman
     has_many :questions, -> { order :sort_order }
     
     validates_presence_of :survey_template_id, :step
+    validates_uniqueness_of :step, scope: :survey_template_id
     
     def self.by_step(step)
       where(step: step)
