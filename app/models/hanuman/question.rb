8305efb6d502fdb009cb44c55e3c4a7eb87c46e4
Kristen Hazard <kristen@hazardbio.com>
2015-02-10 14:46:40 -0800

    add blank observation for new question, delete observations when deleting question, delete answer choices too

--- app/models/hanuman/question.rb
+++ app/models/hanuman/question.rb
@@ -4,8 +4,8 @@ module Hanuman
     belongs_to :answer_type
     belongs_to :survey_step
     #has_many :survey_questions
-    has_many :answer_choices
-    has_many :observations
+    has_many :answer_choices, dependent: :destroy
+    has_many :observations, dependent: :destroy
     
     validates_presence_of :answer_type_id
     # wait until after migration for these validations
@@ -13,9 +13,26 @@ module Hanuman
     
     validates :question_text, presence: true, unless: :no_text_on_that_answer_type
     
+    after_create :submit_blank_observation_data
+    
     def no_text_on_that_answer_type
       unless answer_type.blank?
         answer_type.name.eql? "line"
+      end
+    end
+    
+    # if survey has data submitted against it, then submit blank data for each
+    # survey for newly added question
+    def submit_blank_observation_data
+      unless survey_step.survey_template.fully_editable
+        surveys = survey_step.survey_template.surveys
+        surveys.each do |s|
+          Observation.create(
+            survey_id: s.id,
+            question_id: self.id,
+            answer: ''
+          )
+        end
       end
     end
   end
