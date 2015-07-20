af79538e1f072122795b7568d07698e3b566d717
Kristen Hazard <kristen@suntouchersoftware.com>
2015-07-20 13:30:02 -0700

    add new columns to answer_type

---
+++ db/migrate/20150717234606_add_columns_to_hanuman_answer_types.rb
@@ -0,0 +1,8 @@
+class AddColumnsToHanumanAnswerTypes < ActiveRecord::Migration
+  def change
+    add_column :hanuman_answer_types, :description, :string
+    add_column :hanuman_answer_types, :descriptive_name, :string
+    add_column :hanuman_answer_types, :has_answer_choices, :boolean
+    add_column :hanuman_answer_types, :external_data_source, :string
+  end
+end
