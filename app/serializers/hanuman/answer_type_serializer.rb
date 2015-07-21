691bca5d4d4b84f16e06ca927c54f07ca88d0501
Kristen Hazard <kristen@suntouchersoftware.com>
2015-07-20 17:39:50 -0700

    add new answer type attributes to serializer

--- app/serializers/hanuman/answer_type_serializer.rb
+++ app/serializers/hanuman/answer_type_serializer.rb
@@ -1,5 +1,6 @@
 module Hanuman
   class AnswerTypeSerializer < ActiveModel::Serializer
-    attributes :id, :name, :status
+    attributes :id, :name, :status, :description, :descriptive_name,
+               :has_answer_choices, :external_data_source
   end
 end
