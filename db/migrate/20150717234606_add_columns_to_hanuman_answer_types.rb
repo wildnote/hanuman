e70adcc3787e836f35878fb6b1142c08b5f1d920
Kristen Hazard <kristen@suntouchersoftware.com>
2015-08-04 19:20:02 -0700

    add default value to migration for has_answer_choices

--- db/migrate/20150717234606_add_columns_to_hanuman_answer_types.rb
+++ db/migrate/20150717234606_add_columns_to_hanuman_answer_types.rb
@@ -2,7 +2,7 @@ class AddColumnsToHanumanAnswerTypes < ActiveRecord::Migration
   def change
     add_column :hanuman_answer_types, :description, :string
     add_column :hanuman_answer_types, :descriptive_name, :string
-    add_column :hanuman_answer_types, :has_answer_choices, :boolean
+    add_column :hanuman_answer_types, :has_answer_choices, :boolean, default: false, null: false
     add_column :hanuman_answer_types, :external_data_source, :string
   end
 end
