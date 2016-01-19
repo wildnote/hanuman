73350dcd7a8c6c2b31447c69722c3cd9b156c821
Kristen Hazard <kristen@suntouchersoftware.com>
2016-01-18 16:20:46 -0800

    add answer_choice_type to serializer

--- app/serializers/hanuman/answer_type_serializer.rb
+++ app/serializers/hanuman/answer_type_serializer.rb
@@ -1,6 +1,6 @@
 module Hanuman
   class AnswerTypeSerializer < ActiveModel::Serializer
-    attributes :id, :name, :status, :description, :descriptive_name,
+    attributes :id, :name, :status, :description, :descriptive_name, :answer_choice_type,
                :has_answer_choices, :post_name, :post_type, :element_type
   end
 end
