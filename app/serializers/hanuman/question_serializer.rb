1061d2f8809ed3dbec7d5c36f6d2d267d4387c2f
Kristen Hazard <kristen@suntouchersoftware.com>
2016-01-27 14:00:08 -0800

    add external_data_source to question serializer

--- app/serializers/hanuman/question_serializer.rb
+++ app/serializers/hanuman/question_serializer.rb
@@ -1,6 +1,6 @@
 module Hanuman
   class QuestionSerializer < ActiveModel::Serializer
-    attributes :id, :question_text, :answer_type_id, :sort_order, :survey_step_id, :ancestry, :required, :hidden
+    attributes :id, :question_text, :answer_type_id, :sort_order, :survey_step_id, :ancestry, :required, :hidden, :external_data_source
     has_many :answer_choices, embed: :ids
   end
 end
