452a3bb3957f0d1dc615907d9d02196821bf40be
Kristen Hazard <kristen@suntouchersoftware.com>
2016-04-20 14:14:50 -1000

    delete conditional logic associated with question when delete a question

--- app/models/hanuman/condition.rb
+++ app/models/hanuman/condition.rb
@@ -8,6 +8,17 @@ module Hanuman
     validates :operator, presence: true
     #validates :answer, presence: true - is empty and is not empty have nil values for answer so we can't validate presene of answer-kdh
 
+    after_destroy :cleanup_rule_if_single_condition
+
     OPERATORS = ["is equal to", "is not equal to", "is empty", "is not empty", "is greater than", "is less than", "starts with", "contains"]
+
+    # when a condition gets deleted, check to see of the rule attached to that condition has other conditions attached to it
+    # if it does do NOT delete, if it doen NOT have any more conditions attached go ahead and delete
+    def cleanup_rule_if_single_condition
+      number_of_condtions = self.rule.conditions
+      if self.rule.conditions.blank?
+        self.rule.destroy
+      end
+    end
   end
 end
