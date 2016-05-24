34ad0608908fe17de44014af3b2ede3ced376c94
Kristen Hazard <kristen@suntouchersoftware.com>
2016-05-24 15:57:26 -0700

    don't worry about a resort on deleted questions

--- app/models/hanuman/question.rb
+++ app/models/hanuman/question.rb
@@ -16,7 +16,6 @@ module Hanuman
     validates :question_text, presence: true, unless: :question_text_not_required
 
     after_create :submit_blank_observation_data
-    #after_destroy :resort_submitted_data
 
     amoeba do
       include_association [:rule, :conditions, :answer_choices]
@@ -67,17 +66,6 @@ module Hanuman
               )
             end
           end
-          # calling save so that before_save method apply_group_sort is called to resort observations after inserting new ones for new questions-kdh
-          s.save
-        end
-      end
-    end
-
-    # because we depend on a group sort field to sort submitted data we need to resort the data after a question has been deleted-kdh
-    def resort_submitted_data
-      unless survey_step.survey_template.fully_editable
-        surveys = survey_step.survey_template.surveys
-        surveys.each do |s|
           # calling save so that before_save method apply_group_sort is called to resort observations after inserting new ones for new questions-kdh
           s.save
         end
