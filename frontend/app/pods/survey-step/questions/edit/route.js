d8d44804bc3ad5ee7650f996b770f6ec27ca3f56
Esteban Arango Medina <marranoparael31@gmail.com>
2016-06-10 15:02:10 -0500

    Question loading state

--- frontend/app/pods/survey-step/questions/edit/route.js
+++ frontend/app/pods/survey-step/questions/edit/route.js
@@ -2,19 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
+    let surveyStep = this.modelFor('survey-step');
     return Ember.RSVP.hash({
       question: this.store.findRecord('question', params.question_id),
-      answerTypes: this.store.findAll('answer-type')
+      answerTypes: this.store.findAll('answer-type'),
+      surveyTemplate: surveyStep.get('surveyTemplate')
     });
+  },
+
+  afterModel: function(models) {
+    return models.question.get('answerChoices');
   },
 
   setupController(controller, models) {
     controller.setProperties(models);
-  },
-
-  actions: {
-    transitionToSurveyStep() {
-      this.transitionTo('survey_step',this.currentModel.question.get('surveyStep'));
-    }
   }
 });
