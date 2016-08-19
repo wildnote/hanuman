4953f2775d1c43b65b3902be353faac1f7980e35
Esteban Arango Medina <marranoparael31@gmail.com>
2016-08-19 14:22:51 -0500

    Add FrontendGenerator for deployment.

---
+++ lib/generators/hanuman/frontend_generator.rb
@@ -0,0 +1,9 @@
+require 'rails/generators/base'
+module Hanuman
+  class FrontendGenerator < Rails::Generators::Base
+    source_root File.expand_path("../../../../", __FILE__)
+    def copy_frontend_folder
+      directory 'frontend', 'frontend'
+    end
+  end
+end
