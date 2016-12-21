422fb5dc52038b1c2ce6adbb912d52683a26fa26
David Simmons <david@suntouchersoftware.com>
2016-12-21 02:44:45 -0800

    tabify survey admin layout

--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -25,6 +25,7 @@ module.exports = function(defaults) {
 
   app.import('bower_components/animate.css/animate.min.css');
   app.import('vendor/bootstrap/dropdown/bootstrap-dropdown.js');
+  app.import('vendor/bootstrap/tab/bootstrap-tab.js');
   app.import('vendor/wildnote/wildnote-helper-list.js');
   return app.toTree();
 };
