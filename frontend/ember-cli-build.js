b411cd06bba65f2a0d3d4e28ebc32f8449347d29
Esteban Arango Medina <marranoparael31@gmail.com>
2017-02-02 00:34:09 -0800

    Upgrade ember.

--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -4,7 +4,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    babel: {
+    "ember-cli-babel": {
       optional: ['es6.spec.symbols'],
       includePolyfill: true
     }
