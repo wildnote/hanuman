9a682e0496c40b88d7b8176e9f7de377c10828da
Esteban Arango Medina <marranoparael31@gmail.com>
2017-02-08 15:32:50 -0500

    WIP #136461803

---
+++ db/migrate/20170208200704_add_has_an_answer_to_hanuman_answer_types.rb
@@ -0,0 +1,16 @@
+class AddGpsDataToHanumanObservationVideos < ActiveRecord::Migration
+  def up
+    add_column :hanuman_answer_types, :has_an_answer, :boolean, default: true
+
+    NO_ANSWER_TYPES =  %w(repeater section helperabove helperbelow line static photo document video)
+    Hanuman::AnswerType.all.each do |answer_type|
+      if NO_ANSWER_TYPES.include? answer_type.name
+        answer_type.update_attribute(:has_an_answer, false)
+      end
+    end
+  end
+
+  def down
+    remove_column :hanuman_answer_types, :has_an_answer
+  end
+end
