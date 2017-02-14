6416f504e2848b63018f7c8fab218f00f2bcae61
Esteban Arango Medina <marranoparael31@gmail.com>
2017-02-14 09:54:23 -0500

    #139387951 exclude folder from generator

--- lib/generators/hanuman/frontend_generator.rb
+++ lib/generators/hanuman/frontend_generator.rb
@@ -3,7 +3,7 @@ module Hanuman
   class FrontendGenerator < Rails::Generators::Base
     source_root File.expand_path("../../../../", __FILE__)
     def copy_frontend_folder
-      directory 'frontend', 'frontend'
+      directory 'frontend', 'frontend', {exclude_pattern: /bower_components|node_modules|tmp|testem.log/}
     end
   end
 end
