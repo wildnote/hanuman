8bc5e0179f1379bd85a8e64b76776d269d20f526
Esteban Arango Medina <marranoparael31@gmail.com>
2017-05-11 14:58:41 -0500

    Unscope observation when defining relations.

--- app/models/hanuman/observation_answer.rb
+++ app/models/hanuman/observation_answer.rb
@@ -3,9 +3,10 @@ module Hanuman
     has_paper_trail
 
     # Relations
-    belongs_to :observation, touch: true
-    belongs_to :answer_choice
-    belongs_to :multiselectable, polymorphic: true
+    belongs_to  :observation, touch: true
+    has_one     :survey, through: :observation
+    belongs_to  :answer_choice
+    belongs_to  :multiselectable, polymorphic: true
 
     def answer_choice_text
       answer_choice.option_text
