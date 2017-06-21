daf462d8e7bb4c238f39706f4c95b0f3d4c1e0c0
Esteban Arango Medina <marranoparael31@gmail.com>
2017-06-21 10:38:09 -0500

    Using exam with chrome.

--- frontend/testem.js
+++ frontend/testem.js
@@ -3,6 +3,8 @@ module.exports = {
   "framework": "qunit",
   "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
+  "parallel": -1,
+  "reporter": "dot",
   "launch_in_ci": [
     "PhantomJS"
   ],
