227a8b2ebd8bfcd5284c5f37725a7b54224d7c42
Kristen Hazard <Kristen@Kristens-MBP.domain>
2017-08-09 16:47:39 -0700

    fix bug with taxon lists on native apps, retrieve key for list hash from data_source table not external_data_source string on question table

--- app/serializers/hanuman/question_serializer.rb
+++ app/serializers/hanuman/question_serializer.rb
@@ -5,9 +5,15 @@ module Hanuman
                :capture_location_data
     has_many :answer_choices, embed: :ids
     has_one :rule
+
     # this is a hack to be able to temporarily display question_id in admin to help with development
     def rails_id
       object.id
+    end
+
+    # retrive data source name from data source table now instead of string external_data_source stored in question table-kdh
+    def external_data_source
+      objects.data_source.reference
     end
   end
 end
