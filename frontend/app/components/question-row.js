09daed6ffa5165514a722378d19e6f54aab26a27
Esteban Arango Medina <marranoparael31@gmail.com>
2018-03-12 21:38:17 -0700

    functional selecting

--- frontend/app/components/question-row.js
+++ frontend/app/components/question-row.js
@@ -1,10 +1,16 @@
-import $ from 'jquery';
-import { htmlSafe } from '@ember/string';
 import Component from '@ember/component';
+import { htmlSafe } from '@ember/string';
 import { computed } from '@ember/object';
+import $ from 'jquery';
 
 export default Component.extend({
   attributeBindings: ['question.id:data-question-id'],
+
+  isSelected: computed('selectedQuestions.[]', 'question.id', function() {
+    let selectedQuestions = this.get('selectedQuestions');
+    let questionId = this.get('question.id');
+    return selectedQuestions.some((question) => question.get('id') === questionId);
+  }),
 
   typeInitial: computed('question.{ancestry,hidden,required,rule.isNew}', function() {
     let question = this.get('question');
@@ -36,6 +42,10 @@ export default Component.extend({
       let question = this.get('question');
       let el = this.get('element');
       this.sendAction('deleteQuestion', question, el);
+    },
+    toggleQuestion() {
+      let question = this.get('question');
+      this.get('toggleQuestion')(question);
     }
   }
 });
