7ac45cd842d56aca690d7674908e6a8690737420
Esteban Arango Medina <marranoparael31@gmail.com>
2018-05-28 11:48:04 -0500

    tests for tag actions

--- frontend/mirage/config.js
+++ frontend/mirage/config.js
@@ -11,11 +11,14 @@ export default function() {
   this.del('/survey_templates/:id');
   this.put('/survey_templates/:id', ({ surveyTemplates }, request) => {
     let attrs = JSON.parse(request.requestBody)['survey_template'],
-        id = request.params.id;
+      id = request.params.id;
     return surveyTemplates.find(id).update(attrs);
   });
-  this.get('survey_templates/:id/available_tags', () => {
-    return new Mirage.Response(200, {}, { tags: []});
+  this.get('survey_templates/:id/available_tags', ({ questions }, request) => {
+    let tags = questions.all().models.map((question) => question.tag_list.split(','));
+    tags = tags.flatten().filter(Boolean);
+    tags = [...new Set(tags)];
+    return new Mirage.Response(200, {}, { tags });
   });
 
   // Conditions
@@ -24,14 +27,14 @@ export default function() {
   this.del('/conditions/:id');
   this.put('/conditions/:id', ({ conditions }, request) => {
     let attrs = JSON.parse(request.requestBody)['condition'],
-        id = request.params.id;
+      id = request.params.id;
     return conditions.find(id).update(attrs);
   });
   // Rules
   this.post('/rules');
   this.put('/rules/:id', ({ rules }, request) => {
     let attrs = JSON.parse(request.requestBody)['rule'],
-        id = request.params.id;
+      id = request.params.id;
     return rules.find(id).update(attrs);
   });
   // Questions
@@ -44,7 +47,7 @@ export default function() {
   this.del('/questions/:id');
   this.put('/questions/:id', ({ questions }, request) => {
     let attrs = JSON.parse(request.requestBody)['question'],
-        id = request.params.id;
+      id = request.params.id;
     delete attrs.rule;
     return questions.find(id).update(attrs);
   });
@@ -54,7 +57,7 @@ export default function() {
   this.del('/answer_choices/:id');
   this.put('/answer_choices/:id', ({ answerChoices }, request) => {
     let attrs = JSON.parse(request.requestBody)['answer_choice'],
-        id = request.params.id;
+      id = request.params.id;
     return answerChoices.find(id).update(attrs);
   });
   // Data Sources
@@ -63,11 +66,10 @@ export default function() {
 
   // Custom Organizations
   this.get('/organizations', () => {
-    return new Mirage.Response(200, {}, { organizations: []});
+    return new Mirage.Response(200, {}, { organizations: [] });
   });
 
   this.get('/survey_template_export_types', () => {
-    return new Mirage.Response(200, {}, { survey_template_export_types: []});
+    return new Mirage.Response(200, {}, { survey_template_export_types: [] });
   });
-
 }
--- frontend/tests/acceptance/bulk-tagging-test.js
+++ frontend/tests/acceptance/bulk-tagging-test.js
@@ -25,3 +25,53 @@ test('when there no available tags', async function(assert) {
   await fillIn('[data-test-tag-input-search]', 'a new tag');
   assert.equal(find('[data-test-new-tag-label]').text(), 'a new tag', 'new tag text');
 });
+
+test('deselecting tags', async function(assert) {
+  assert.expect(9);
+
+  server.create('question', {
+    surveyTemplate,
+    tag_list: 'cars,dogs'
+  });
+
+  server.create('question', {
+    surveyTemplate,
+    tag_list: 'colors,dogs'
+  });
+
+  await visit(`/survey_templates/${surveyTemplate.id}`);
+  await find('[data-test-question-row] [data-test-question-selector]')[0].click();
+  await find('[data-test-question-row] [data-test-question-selector]')[1].click();
+  await click('[data-test-open-tagging-modal]');
+
+  assert.equal(find('[data-test-tagging-modal]').length, 1, 'tagging modal is shown');
+  await waitUntil(() => find('[data-test-tag-input-search]:enabled').length);
+  assert.equal(find('[data-test-apply-tag-changes]').hasClass('disabled'), true, 'apply btn is disabled');
+  await waitUntil(() => find('[data-test-tag-list]').length);
+
+  assert.equal(
+    find('[data-test-tagging-row="cars"] input').prop('indeterminate'),
+    true,
+    'cars tagg is partially selected'
+  );
+
+  assert.equal(
+    find('[data-test-tagging-row="colors"] input').prop('indeterminate'),
+    true,
+    'colors tagg is partially selected'
+  );
+
+  assert.equal(find('[data-test-tagging-row="dogs"] input').prop('checked'), true, 'dogs tagg is fully selected');
+
+  await click('[data-test-tagging-row="cars"]'); // add to all
+  assert.equal(find('[data-test-apply-tag-changes]').hasClass('disabled'), false, 'apply btn is enabled');
+  await click('[data-test-tagging-row="cars"]'); // deselect to all
+  await click('[data-test-tagging-row="cars"]'); // initital state
+  assert.equal(find('[data-test-apply-tag-changes]').hasClass('disabled'), true, 'apply btn is disabled');
+  await click('[data-test-tagging-row="cars"]');
+  await click('[data-test-tagging-row="cars"]');
+  await click('[data-test-apply-tag-changes]');
+  server.schema.questions.all().models.forEach((question) => {
+    assert.equal(question.tag_list.includes('cars'), false, "doesn't include `cars`");
+  });
+});
