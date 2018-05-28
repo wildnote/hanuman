b23b9fa1d590a9f8890960a812d536aefd47b622
Esteban Arango Medina <marranoparael31@gmail.com>
2018-05-28 16:30:57 -0500

    #157849170 / Add missing level classes

--- app/serializers/hanuman/question_serializer.rb
+++ app/serializers/hanuman/question_serializer.rb
@@ -3,8 +3,8 @@ module Hanuman
     attributes :id, :question_text, :answer_type_id, :sort_order,
                :ancestry, :required, :hidden, :external_data_source, :rails_id, :parent_id,
                :capture_location_data, :combine_latlong_as_line, :combine_latlong_as_polygon,
-               :enable_survey_history, :new_project_location
-               :enable_survey_history, :layout_section, :layout_row, :layout_column, :layout_column_position
+               :enable_survey_history, :new_project_location, :layout_section, :layout_row,
+               :layout_column, :layout_column_position
 
     has_many :answer_choices, embed: :ids
     has_one :rule
