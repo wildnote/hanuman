26eb87868a48e58ca66ff166d4954a63da7b73df
Alex Nielsen <alex@suntouchersoftware.com>
2018-06-05 12:25:44 +1000

    check and use project ID in survey show link if available

--- frontend/app/pods/survey-templates/record/controller.js
+++ frontend/app/pods/survey-templates/record/controller.js
@@ -3,6 +3,8 @@ import { alias } from '@ember/object/computed';
 
 export default Controller.extend({
   isLoadingQuestions: true,
-  surveyTemplate: alias('model')
+  surveyTemplate: alias('model'),
+  hasProjectId: window.location.href.indexOf('/projects/') !== -1,
+  projectId: window.location.href.split('/')[6]
 });
 
