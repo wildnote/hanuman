49cdb4b5d429207b5679ff4a56a4b4b6ba98c827
Alex Nielsen <alex@suntouchersoftware.com>
2018-06-12 15:41:33 +1000

    fixes for observation signature model

--- db/migrate/20180612042207_create_hanuman_observation_signatures.rb
+++ db/migrate/20180612042207_create_hanuman_observation_signatures.rb
@@ -1,7 +1,7 @@
 class CreateHanumanObservationSignatures < ActiveRecord::Migration
   def change
     create_table :hanuman_observation_signatures do |t|
-      t.references :observation, index: true, foreign_key: foreign_key: { on_delete: :cascade }
+      t.references :observation, index: true, foreign_key: { on_delete: :cascade }
       t.string :signature
 
       t.timestamps null: false
