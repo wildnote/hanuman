ef49da8913cf0c9c0dc8aeb8675a5197943ce87e
Alex Nielsen <alex@suntouchersoftware.com>
2018-06-13 10:17:01 +1000

    removed unneeded generated signature spec

--- spec/factories/hanuman_observation_signatures.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :hanuman_observation_signature, class: 'Hanuman::ObservationSignature' do
-    observation nil
-    signature "MyString"
-  end
-end
--- spec/models/hanuman/observation_signature_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-module Hanuman
-  RSpec.describe ObservationSignature, type: :model do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-end
