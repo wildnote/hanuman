b9a9796e2c745acb8610fb2bf55d7d38df466ab1
Kristen Hazard <kristen@suntouchersoftare.com>
2018-12-20 16:11:33 -0800

    experiment with different approach to dependent and getting back papertrail destroy for survey_extension

--- app/models/hanuman/survey.rb
+++ app/models/hanuman/survey.rb
@@ -11,7 +11,7 @@ module Hanuman
     has_many :observation_documents, through: :unscope_observations
     has_many :observation_photos, through: :unscope_observations
     has_many :observation_videos, through: :unscope_observations
-    has_one :survey_extension, dependent: :delete
+    has_one :survey_extension, dependent: :destroy
     accepts_nested_attributes_for :survey_extension, allow_destroy: true
 
     attr_accessor :should_schedule_sort, :skip_sort
@@ -170,8 +170,8 @@ module Hanuman
               if trigger_observation.observation_answers.present?
                 cond_met = false
                 trigger_observation.observation_answers.each do |obs_answer|
-                  cond_met = 
-                    (obs_answer.answer_choice.present? && obs_answer.answer_choice.option_text == cond.answer) || 
+                  cond_met =
+                    (obs_answer.answer_choice.present? && obs_answer.answer_choice.option_text == cond.answer) ||
                     (obs_answer.taxon.present? && obs_answer.taxon.formatted_answer_choice_with_symbol == cond.answer)
                   break if cond_met
                 end
@@ -188,13 +188,13 @@ module Hanuman
                 cond_met = true
                 trigger_observation.observation_answers.each do |obs_answer|
                   if obs_answer.answer_choice_text == cond.answer
-                    is_equal_to = 
-                      (obs_answer.answer_choice.present? && obs_answer.answer_choice.option_text == cond.answer) || 
+                    is_equal_to =
+                      (obs_answer.answer_choice.present? && obs_answer.answer_choice.option_text == cond.answer) ||
                       (obs_answer.taxon.present? && obs_answer.taxon.formatted_answer_choice_with_symbol == cond.answer)
 
                     if is_equal_to
-                      cond_met = false 
-                      break  
+                      cond_met = false
+                      break
                     end
                   end
                 end
@@ -229,8 +229,8 @@ module Hanuman
               if trigger_observation.observation_answers.present?
                 cond_met = false
                 trigger_observation.observation_answers.each do |obs_answer|
-                  cond_met = 
-                    (obs_answer.answer_choice.present? && obs_answer.answer_choice.option_text.include?(cond.answer)) || 
+                  cond_met =
+                    (obs_answer.answer_choice.present? && obs_answer.answer_choice.option_text.include?(cond.answer)) ||
                     (obs_answer.taxon.present? && obs_answer.taxon.formatted_answer_choice_with_symbol.include?(cond.answer))
                   break if cond_met
                 end
@@ -252,7 +252,7 @@ module Hanuman
           end
 
           obs.hidden = !(rule.match_type == "any" ? condition_results.any? : condition_results.all?)
-          
+
           if obs.hidden && obs.question.has_children?
             obs.hide_tree!
           end
