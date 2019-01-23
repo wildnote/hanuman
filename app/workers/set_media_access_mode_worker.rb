377c5fec010c617184131286753a54d4ff7b2d4b
kaioneill <oneillkai@icloud.com>
2019-01-23 15:42:30 -0800

    #160002343 videos working, excel not

--- app/workers/set_media_access_mode_worker.rb
+++ app/workers/set_media_access_mode_worker.rb
@@ -11,12 +11,12 @@ class SetMediaAccessModeWorker
     when "video"
       media = Hanuman::ObservationVideo.find media_id
       unless media.video.blank?
-        Cloudinary::Api.update_resources_access_mode_by_ids('authenticated', media.video.my_public_id)
+        Cloudinary::Api.update_resources_access_mode_by_ids('authenticated', media.video.my_public_id, :resource_type => :video)
       end
     when "document"
       media = Hanuman::ObservationDocument.find media_id
       unless media.document.blank?
-        Cloudinary::Api.update_resources_access_mode_by_ids('authenticated', media.document.my_public_id)
+        Cloudinary::Api.update_resources_access_mode_by_ids('authenticated', media.document.my_public_id, :resource_type => :raw)
       end
     when "signature"
       media = Hanuman::ObservationSignature.find media_id
