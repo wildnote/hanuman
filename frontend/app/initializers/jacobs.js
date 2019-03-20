8197e10deaa5185f4bfb53da47cfe0dfa9eec820
Kai O'Neill <kai@wildnoteapp.com>
2019-03-20 10:15:26 -0700

    #164749484 intermediate to get banner js working in admin

--- frontend/app/initializers/jacobs.js
+++ frontend/app/initializers/jacobs.js
@@ -1,6 +1,22 @@
 if (!window.jacobs) {
   window.jacobs = false;
 }
+
+$('.hide_announcement').click(function () {
+  var a_id = $('.hide_announcement.announcement-close').attr("data-announcementid");
+  // createCookie("announcement_" + a_id, "hidden", 45);
+  //$(this).parent().slideDown();
+  $('announce').first().slideUp();
+
+  $.ajax({
+    url: "/users/read_latest_terms/",
+    type: "GET",
+    error: function (error) {
+      console.log(error)
+    },
+  });
+});
+
 export function initialize(application) {
   application.register('global:jacobs', window.jacobs, { instantiate: false });
   application.inject('route', 'isJacobs', 'global:jacobs');
