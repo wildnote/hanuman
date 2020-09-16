94597e4dc1a41e8a9fbc08fd9a422c60790fb5ed
nick-wildnoteapp <nick@wildnoteapp.com>
2020-09-16 15:13:34 -0700

    #174852518 intermediate commit - add survey after_commit methods to sort observations worker

--- app/workers/sort_observations_worker.rb
+++ app/workers/sort_observations_worker.rb
@@ -3,13 +3,28 @@ class SortObservationsWorker
 
   def perform(survey_id)
     survey = Hanuman::Survey.find(survey_id)
-    
-    unless survey.observations_sorted
-      survey.sort_observations!
+
+    if survey.wetland_v2_web_v3? 
+      survey.set_wetland_dominant_species
     end
 
-    unless survey.observation_visibility_set
-      survey.set_observation_visibility!
+    if survey.mobile_v3_or_higher?
+      survey.sort_veg_repeaters
+    end
+
+    if survey.web_wetland_v3_v4?
+      survey.set_dominance_test
+      survey.set_rapid_test_hydrophytic
+    end
+
+    if survey.should_schedule_sort?
+      unless survey.observations_sorted
+        survey.sort_observations!
+      end
+
+      unless survey.observation_visibility_set
+        survey.set_observation_visibility!
+      end
     end
   end
 end
