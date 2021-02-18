64bcf49310ab2b7cc8db257375016298e175e257
Alex Nielsen <alex@wildnoteapp.com>
2021-02-18 09:10:01 -0800

    W5 - survey admin changes for answer choice definitions

--- app/serializers/hanuman/answer_choice_serializer.rb
+++ app/serializers/hanuman/answer_choice_serializer.rb
@@ -1,6 +1,6 @@
 module Hanuman
   class AnswerChoiceSerializer < ActiveModel::Serializer
-    attributes :id, :option_text, :scientific_text, :question_id, :group_text, :sort_order
+    attributes :id, :option_text, :scientific_text, :question_id, :group_text, :sort_order, :definition
 
     def group_text
       object.parent.option_text unless object.parent.blank?
