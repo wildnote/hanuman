b4de7a70b7fdd42f8fa2b168cc88a9aee35f586e
Kristen Hazard <kristen@wildnoteapp.com>
2023-02-28 12:20:58 -0800

    1398 - fix again method to return has_calc_engine_calcs?

--- app/models/hanuman/survey_template.rb
+++ app/models/hanuman/survey_template.rb
@@ -44,7 +44,7 @@ module Hanuman
       questions.each do |q|
         if q.is_calc_engine_calculated?
           has_calcs = true
-          return
+          return true
         end
       end
       return has_calcs
