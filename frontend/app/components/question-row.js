1cd2d4640af3465941622b19f2276179f6a083bc
Kristen Hazard <kristen@wildnoteapp.com>
2024-10-08 11:12:24 -0700

    2101 - add default answer to form designer question list

--- frontend/app/components/question-row.js
+++ frontend/app/components/question-row.js
@@ -19,7 +19,7 @@ export default Component.extend({
     return selectedQuestions.some((question) => question.get('id') === questionId);
   }),
 
-  typeInitial: computed('question.{ancestry,hidden,required,visibilityRule.isNew,displayDataInRepeaterHeader}', function() {
+  typeInitial: computed('question.{ancestry,hidden,required,visibilityRule.isNew,displayDataInRepeaterHeader,defaultAnswer}', function() {
     let question = this.get('question');
     let intial = '';
     if (question.get('hidden')) {
@@ -36,6 +36,9 @@ export default Component.extend({
     }
     if (question.get('displayDataInRepeaterHeader')) {
       intial += `<span class="label label-default">Display</span>`
+    }
+    if (question.get('defaultAnswer')) {
+      intial += `<span class="label label-default">Default</span>`
     }
     console.log(intial);
     return htmlSafe(intial);
