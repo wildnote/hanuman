b91ae1cdce03ad38863923f06d277c9816bde4c7
Kristen Hazard <kristen@wildnoteapp.com>
2025-03-05 17:46:51 -0800

    2251 - working through rails 5.2 serializer issues

--- app/serializers/hanuman/answer_choice_serializer.rb
+++ app/serializers/hanuman/answer_choice_serializer.rb
@@ -1,5 +1,8 @@
 module Hanuman
   class AnswerChoiceSerializer < ActiveModel::Serializer
+    def json_key
+      'answer_choice'
+    end
     attributes :id, :option_text, :scientific_text, :question_id, :group_text, :sort_order, :definition
 
     def group_text
