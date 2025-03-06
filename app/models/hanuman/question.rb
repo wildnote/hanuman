ea12f86339263c01ba46f0489c601d02b89b7387
Kristen Hazard <kristen@wildnoteapp.com>
2025-03-05 20:41:44 -0800

    2251 - need to add so that form dup can work

--- app/models/hanuman/question.rb
+++ app/models/hanuman/question.rb
@@ -8,7 +8,7 @@ module Hanuman
 
     # Relations
     belongs_to :answer_type
-    belongs_to :survey_template
+    belongs_to :survey_template, optional: true
     has_many :answer_choices, -> { order :sort_order, :option_text }, dependent: :destroy, inverse_of: :question
     # if a user deletes a question from survey admin we need to delete related observations, giving warning in survey admin
     has_many :observations, dependent: :destroy #**** controlling the delete through a confirm on the ember side of things-kdh *****
