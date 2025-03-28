117a0251cdcab0c86607900a147e1a13082e5a69
Kristen Hazard <kristen@wildnoteapp.com>
2025-03-28 14:51:03 -0700

    node 16, ember-cli-rails .12

--- app/controllers/hanuman/admin_controller.rb
+++ app/controllers/hanuman/admin_controller.rb
@@ -2,6 +2,7 @@ require_dependency "hanuman/application_controller"
 
 module Hanuman
   class AdminController < ApplicationController
+    include EmberCli::Helpers
     layout 'admin'
 
     def index
