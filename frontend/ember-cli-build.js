07a86bdc7e59634f67ce7f8a122c453d305df9b3
Kristen Hazard <kristen@wildnoteapp.com>
2025-03-27 19:07:06 -0700

    Build Error (broccoli-persistent-filter:Babel > [Babel: frontend]) in spin.js

--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -10,12 +10,6 @@ module.exports = function(defaults) {
       includePolyfill: true,
       compileModules: true,
       plugins: [
-        [require.resolve('babel-plugin-module-resolver'), {
-          root: ['./app'],
-          alias: {
-            'spin': 'vendor/ladda/spin'
-          }
-        }],
         require.resolve('babel-plugin-transform-object-rest-spread')
       ]
     },
@@ -45,6 +39,5 @@ module.exports = function(defaults) {
   app.import('vendor/bootstrap/popover/bootstrap-popover.js');
   app.import('vendor/wildnote/wildnote-helper-list.js');
   app.import('vendor/rails-ujs-handlers.js');
-  app.import('vendor/ladda/spin.js');
   return app.toTree();
 };
