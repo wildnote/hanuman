ca17d21d1622ffb9d284521002c52c64fb964672
Kristen Hazard <kristen@wildnoteapp.com>
2025-03-31 21:03:03 -0700

    updating ember-cli-babel then not

--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -11,8 +11,7 @@ module.exports = function(defaults) {
       compileModules: true,
       plugins: [
         require.resolve('babel-plugin-transform-object-rest-spread')
-      ],
-      exclude: ['transform-es2015-block-scoping', 'transform-es2015-typeof-symbol']
+      ]
     },
     eslint: {
       testGenerator: 'qunit',
