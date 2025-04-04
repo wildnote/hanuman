634d95e67b6339150d93b53fb0b6395ff0701283
Kristen Hazard <kristen@wildnoteapp.com>
2025-04-04 14:09:39 -0700

    spinner js error, not sure spinner actually works

---
+++ frontend/app/components/css-spinner.js
@@ -0,0 +1,22 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  classNames: ['css-spinner'],
+  
+  // Optional properties that can be passed in
+  size: 40,
+  color: '#3498db',
+  
+  didInsertElement() {
+    this._super(...arguments);
+    
+    // Apply custom size and color if provided
+    if (this.size) {
+      this.element.style.setProperty('--spinner-size', `${this.size}px`);
+    }
+    
+    if (this.color) {
+      this.element.style.setProperty('--spinner-color', this.color);
+    }
+  }
+});
